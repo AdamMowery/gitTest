3b70f406d1c8e887095c6e8bd087b4948a8bde9a
Adam <adammowery7@gmail.com>
2017-01-31 11:47:38 -0500

    bonus3 initial add

---
+++ bonus3/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        boolean play = true;
+        int randNumber = new Random().nextInt(100 + 1);
+        while (play) {
+
+            System.out.println();
+
+
+        }
+    }
+}
